dd8e15611d5c00a295d6af91e1e19d418231403e
Flostin1 <34432412+Flostin1@users.noreply.github.com>
2018-11-21 20:45:11 -0600

    Adding Checkers
    This commit is adding a folder that holds images and java classes for a game of checkers.

---
+++ myfirstgame/Java Projects/Checkers/Main.java
@@ -0,0 +1,100 @@
+/**
+ * Credits:
+ * * Checker Board Image: https://www.souvnear.com/mnt-storage/products/Amritsar/FD-ASR-015/2.jpg
+ * * Checker Pieces: https://studio.code.org/projects/applab/qOrtceIfe4F4g3q5NDoLfm0GYaN2iuIJL0rN4cA_-hY
+ */
+
+import java.io.File;
+import java.io.IOException;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+    static JFrame frame;
+    static DrawPanel canvas;
+
+    // The width and height of the canvas
+    private int width = 600;
+    private int height = 600;
+
+    private Piece[] checkerPieces;
+
+    // Image files...
+    private BufferedImage checkerBoard = loadImage("Checker_Board.jpg");
+    
+    public static void main(String[] args) throws IOException {
+        new Main().init();
+    }
+
+    // Sets up the canvas and puts it into a JFrame
+    private void init() throws IOException {
+        frame = new JFrame("Checkers");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        canvas = new DrawPanel();
+        checkerPieces = Piece.generatePieces();
+
+        frame.getContentPane().add(BorderLayout.CENTER, canvas);
+
+        frame.setResizable(false);
+        frame.setLocationByPlatform(true);
+        frame.setVisible(true);
+        frame.pack();
+        loop();
+    }
+
+    // Code to repeat...
+    private void loop() {
+        while (true) {
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            frame.repaint();
+        }
+    }
+
+    // This function takes a filename and returns a BufferedImage
+    public static BufferedImage loadImage(String filename) {
+        try {
+            return ImageIO.read(new File(filename));
+        } catch(IOException e) {
+            e.printStackTrace();
+            System.out.println("File '" + filename + "' not found.");
+        }
+
+        return null;
+    }
+
+    class DrawPanel extends JPanel {
+        private static final long serialVersionUID = 1L;
+
+        // Sets up the canvas
+        public DrawPanel() {
+            setOpaque(true);
+            setBackground(Color.WHITE);
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(width, height);
+        }
+
+        @Override
+        // Code to be drawn...
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+        }
+    }
+}
